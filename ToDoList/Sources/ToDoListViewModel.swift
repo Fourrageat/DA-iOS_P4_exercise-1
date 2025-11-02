bd34232e06ad2a6ef80d7a180454c88b7610ad6b
Fourrageat <baptiste.fourrageat@icloud.com>
2025-11-02 12:40:33 +0100

    implemented filter todolist logic

--- ToDoList/Sources/ToDoListViewModel.swift
+++ ToDoList/Sources/ToDoListViewModel.swift
@@ -4,12 +4,16 @@ final class ToDoListViewModel: ObservableObject {
     // MARK: - Private properties
 
     private let repository: ToDoListRepositoryType
+    private var toFilterToDoItems: [ToDoItem]
+    private var currentFilterIndex: Int = 0
 
     // MARK: - Init
 
     init(repository: ToDoListRepositoryType) {
         self.repository = repository
-        self.toDoItems = repository.loadToDoItems()
+        let loadedToDoItems = repository.loadToDoItems()
+        self.toFilterToDoItems = loadedToDoItems
+        self.toDoItems = loadedToDoItems
     }
 
     // MARK: - Outputs
@@ -17,7 +21,9 @@ final class ToDoListViewModel: ObservableObject {
     /// Publisher for the list of to-do items.
     @Published var toDoItems: [ToDoItem] = [] {
         didSet {
-            repository.saveToDoItems(toDoItems)
+            print("apres")
+            print(toFilterToDoItems)
+            repository.saveToDoItems(toFilterToDoItems)
         }
     }
 
@@ -25,30 +31,34 @@ final class ToDoListViewModel: ObservableObject {
 
     // Add a new to-do item with priority and category
     func add(item: ToDoItem) {
-        toDoItems.append(item)
+        toFilterToDoItems.append(item)
+        applyFilter(at: currentFilterIndex)
     }
 
     /// Toggles the completion status of a to-do item.
     func toggleTodoItemCompletion(_ item: ToDoItem) {
-        if let index = toDoItems.firstIndex(where: { $0.id == item.id }) {
-            toDoItems[index].isDone.toggle()
+        if let index = toFilterToDoItems.firstIndex(where: { $0.id == item.id }) {
+            toFilterToDoItems[index].isDone.toggle()
+            applyFilter(at: currentFilterIndex)
         }
     }
 
     /// Removes a to-do item from the list.
     func removeTodoItem(_ item: ToDoItem) {
-        toDoItems.removeAll { $0.id == item.id }
+        toFilterToDoItems.removeAll { $0.id == item.id }
+        applyFilter(at: currentFilterIndex)
     }
 
     /// Apply the filter to update the list.
-    func applyFilter(at index: Int) {
-        switch index {
-        case 1:
-            print("done")
-        case 2:
-            print("not done")
-        default:
-            print("all")
+    func applyFilter(at filterIndex: Int) {
+        currentFilterIndex = filterIndex
+        switch filterIndex {
+        case 1: // Done
+            toDoItems = toFilterToDoItems.filter { $0.isDone }
+        case 2: // Not Done
+            toDoItems = toFilterToDoItems.filter { !$0.isDone }
+        default: // All
+            toDoItems = toFilterToDoItems
         }
     }
 }
